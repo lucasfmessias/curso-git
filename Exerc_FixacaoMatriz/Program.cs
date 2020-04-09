cae77727ff039be701180ffa95abf575b1084d79
Lucas Messias <lucas-messias1993@bol.com.br>
2020-04-09 14:25:09 -0300

    Projeto uso de matriz

---
+++ Exerc_FixacaoMatriz/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exerc_FixacaoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(s[0]);
+            int m = int.Parse(s[1]);
+
+            int[,] mat = new int[n, m];
+
+            // Loading data in matrix
+            for (int i = 0; i < n; i++)
+            {
+                string[] sVetor = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    mat[i, j] = int.Parse(sVetor[j]);
+                }
+            }
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mat[i, j] == num)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+
+                        if (j - 1 >= 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (j + 1 < m)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i - 1 >= 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (i + 1 < n) // n = Linha
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
